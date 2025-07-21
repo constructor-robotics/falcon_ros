fc50613480f1f0be98851db39a7934376739350a
v4hn <me@v4hn.de>
2025-07-21 04:31:30 +0200

    general overhaul
    delete everything redundant
    strongly improve the code quality of the core driver.
    
    use crtk conventions.
    
    There is a lot to do to make this really useful,
    most prominently there is no servo_cp support yet,
    but that requires a working controller.
    
    Secondly it is not clear to me right now how gravity compensation would/does work.

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # Description
-This package allows ROS interface with the Novint Falcon Device.
 
-## 1. Authors
-Steven Martin:
+OpenSource ROS interfaces for the Novint Falcon Device.
 
-Adnan Munawar: amunawar@wpi.edu
+This package utilizes the open libnifalcon project and the code is based on contributions by Michael Goerner, Steven Martin, and Adnan Munawar.
+
+Alternative drivers with controller integration which rely on the proprietary force dimensions SDK:
+- https://github.com/jhu-saw/sawForceDimensionSDK
+- https://github.com/ICube-Robotics/forcedimension_ros2
 
 ## 2. Dependencies
-Tested on ROS Hydro, Indigo and Melodic. For this package to work, please download, build and install the `libnifalcon` drivers from:
+
+Tested on ROS in Debian trixie. For this package to work, please download, build and install the `libnifalcon` drivers from:
 
 https://github.com/libnifalcon/libnifalcon
  
 ## 3. Setting the permissions
+
 Setting the udev permissions is required. The following commands can be used for this purpose: 
 ```bash
 roscd ros_falcon
@@ -30,6 +34,7 @@ Make sure that the power cord in plugged into the Falcon and the Falcon is conne
 ```bash
 findfalcons
 ```
+
 You should see the Falcon's LEDs change color and the terminal should indicated whether the Falcon is ready or not. If the program fails, running it a few times helps. You may need to run the program with `sudo` if the `udev` permissions were not set properly. However, you would need the `udev` set correctly for the ROS pacakge to work.
 
 **IMPORTANT: YOU MAY ALSO NEED TO HOME THE FALCON WHILE THE PROGRAM IS RUNNING. READ SECTION 4.1 ON HOW TO DO THAT**
@@ -41,8 +46,8 @@ barrow_mechanics
 ```
 
 ### 4.1 Homing the Falcon:
-The Falcon is homed by running any utility program that accesses the Falcon, then pulling the knob all the way outwards and then pushing it inwards until you sense a locking force. At this point, the falcon is homed.
 
+The Falcon is homed by running any utility program that accesses the Falcon, then pulling the knob all the way outwards and then pushing it inwards until you sense a locking force. At this point, the falcon is homed.
 
 If these steps work, then you should be able to use the ROS pacakge.
 
@@ -51,16 +56,5 @@ If these steps work, then you should be able to use the ROS pacakge.
 ```
 rosrun ros_falcon driver
 ```
+
 This program should spawn the required ROS topics for state/command of the Falcon device.
-
-## 5. Known Issues:
-
-### 5.1 Moving the Falcon does not change the state data
-You may need to home the Falcon as described in 4.1
-
-### 5.2 Library not found
-
-For the following error, ``Error while loading shared libraries: libnifalcon.so.1.0.2: cannot open shared object file: No such file or directory``
-
-Try running `sudo ldconfig`
-
--- src/driver.cpp
+++ src/driver.cpp
@@ -1,197 +1,193 @@
 //////////////////////////////////////////////////////////
-// ROSfalcon Driver. Publishes and subscribes to falconMsgs for Novint Falcon.
+// Novint Falcon ROS Driver.
 //
-// Using LibniFalcon 
-// Steven Martin
-// Based on barrow_mechanics example by Alistair Barrow
+// 2025 Michael Goerner
+// based on code by Steven Martin
+// based on barrow_mechanics example by Alistair Barrow
 
-
-#include <iostream>
-#include <string>
-#include <cmath>
 #include <ros/ros.h>
-#include <ros_falcon/falconPos.h>
-#include <ros_falcon/falconForces.h>
+#include <ros/callback_queue.h>
+
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/WrenchStamped.h>
+#include <sensor_msgs/Joy.h>
 
 #include "falcon/core/FalconDevice.h"
 #include "falcon/firmware/FalconFirmwareNovintSDK.h"
-#include "falcon/util/FalconCLIBase.h"
 #include "falcon/util/FalconFirmwareBinaryNvent.h"
-#include "falcon/kinematic/stamper/StamperUtils.h"
+#include "falcon/grip/FalconGripFourButton.h"
+// #include "falcon/util/FalconCLIBase.h"
+// #include "falcon/kinematic/stamper/StamperUtils.h"
 #include "falcon/kinematic/FalconKinematicStamper.h"
-#include "falcon/core/FalconGeometry.h"
-#include "falcon/gmtl/gmtl.h"
-#include "falcon/util/FalconFirmwareBinaryNvent.h"
-
+// #include "falcon/core/FalconGeometry.h"
+// #include "falcon/gmtl/gmtl.h"
 
 using namespace libnifalcon;
-using namespace std;
 using namespace StamperKinematicImpl;
-using namespace ros_falcon;
-FalconDevice m_falconDevice;
 
-/**********************************************
-This function initialises the novint falcon controller
+class Controller : public FalconDevice {
+	ros::CallbackQueue cb_queue_;
+	ros::NodeHandle nh_;
+	ros::Publisher measured_cp_pub_;
+	ros::Publisher joy_pub_;
+	ros::Subscriber servo_cf_sub_;
 
-NoFalcons is the index of the falcon which you wish to initialise
-Index 0 is first falcon.
-**********************************************/
- 
-bool init_falcon(int NoFalcon) 
-
-{
-  	cout << "Setting up LibUSB" << endl;
-    m_falconDevice.setFalconFirmware<FalconFirmwareNovintSDK>(); //Set Firmware
-  	if(!m_falconDevice.open(NoFalcon)) //Open falcon @ index 0  (index needed for multiple falcons, assuming only one connected)
-  	{
-  	    cout << "Failed to find falcon" << endl;
-        return false;
-  	}
-    else
-    {
-        cout << "Falcon Found" << endl;
-    }
-
-    //There's only one kind of firmware right now, so automatically set that.
-	m_falconDevice.setFalconFirmware<FalconFirmwareNovintSDK>();
-	//Next load the firmware to the device
-	
-	bool skip_checksum = false;
-	//See if we have firmware
-	bool firmware_loaded = false;
-	firmware_loaded = m_falconDevice.isFirmwareLoaded();
-	if(!firmware_loaded)
+	std::mutex force_mutex_;
+	std::array<double, 3> cmd_force_ = {0.0, 0.0, 0.0};
+public:
+	Controller(ros::NodeHandle nh) :
+		FalconDevice(),
+		nh_(nh)
 	{
-		cout << "Loading firmware" << endl;
-		uint8_t* firmware_block;
-		long firmware_size;
-		{
+		// set only communication protocol (matching the firmware loaded later)
+		setFalconFirmware<FalconFirmwareNovintSDK>();
+		getFalconFirmware()->setForces(std::array<int, 3>{ 0, 0, 0 });
 
-			firmware_block = const_cast<uint8_t*>(NOVINT_FALCON_NVENT_FIRMWARE);
-			firmware_size = NOVINT_FALCON_NVENT_FIRMWARE_SIZE;
+		// set only kinematic model
+		setFalconKinematic<libnifalcon::FalconKinematicStamper>();
+		setFalconGrip<FalconGripFourButton>();
 
+		nh_.setCallbackQueue(&cb_queue_);
+		measured_cp_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("measured_cp", 1, true);
+		joy_pub_ = nh_.advertise<sensor_msgs::Joy>("joy", 1, true);
+		servo_cf_sub_ = nh_.subscribe<geometry_msgs::WrenchStamped>("servo_cf", 1, [=](auto&& msg) {
+			servo_cf(*msg);
+		});
+	}
 
-			for(int i = 0; i < 20; ++i)
-			{
-				if(!m_falconDevice.getFalconFirmware()->loadFirmware(skip_checksum, NOVINT_FALCON_NVENT_FIRMWARE_SIZE, const_cast<uint8_t*>(NOVINT_FALCON_NVENT_FIRMWARE)))
+	~Controller() {
+		close();
+	}
 
-				{
-					cout << "Firmware loading try failed" <<endl;
-				}
-				else
-				{
-					firmware_loaded = true;
-					break;
-				}
-			}
+	bool init(){
+		if(!open(0)) {
+			ROS_ERROR("Failed to connect to device");
+			return false;
 		}
+
+		bool const skip_checksum = false;
+		while(!isFirmwareLoaded()) {
+			getFalconComm()->setFirmwareMode(); // this fails initially, but loadFirmware succeeds on first try this way
+			if(getFalconFirmware()->loadFirmware(skip_checksum, NOVINT_FALCON_NVENT_FIRMWARE_SIZE, const_cast<uint8_t*>(NOVINT_FALCON_NVENT_FIRMWARE)))
+				break;
+			ROS_ERROR_STREAM("Failed to upload firmware.");
+
+			if(!ros::ok())
+				return false;
+		}
+
+		return true;
 	}
-	else if(!firmware_loaded)
+
+	bool home(){
+		runIOLoop(FALCON_LOOP_FIRMWARE);
+		if(getFalconFirmware()->isHomed()) {
+			ROS_INFO("device is already homed.");
+			return true;
+		}
+
+		getFalconFirmware()->setHomingMode(true);
+		getFalconFirmware()->setLEDStatus(FalconFirmware::BLUE_LED);
+		runIOLoop(FALCON_LOOP_FIRMWARE);
+
+		if(!getFalconFirmware()->isHomed())
+			ROS_INFO("Falcon not currently homed. Move control all the way out then push straight all the way in.");
+
+		ros::Rate rate{ 100 };
+		while(!getFalconFirmware()->isHomed())
+		{
+			if (!ros::ok())
+				return false;
+
+			runIOLoop(FALCON_LOOP_FIRMWARE);
+
+			rate.sleep();
+		}
+
+		getFalconFirmware()->setHomingMode(false);
+		ROS_INFO("Homing complete.");
+		return true;
+	}
+
+	void servo_cf(const geometry_msgs::WrenchStamped& msg)
 	{
-		cout << "No firmware loaded to device, and no firmware specified to load (--nvent_firmware, --test_firmware, etc...). Cannot continue" << endl;
-		return false;
+		std::lock_guard<std::mutex> lock(force_mutex_);
+		cmd_force_[0] = msg.wrench.force.x;
+		cmd_force_[1] = msg.wrench.force.y;
+		cmd_force_[2] = msg.wrench.force.z;
 	}
-	if(!firmware_loaded || !m_falconDevice.isFirmwareLoaded())
-	{
-		cout << "No firmware loaded to device, cannot continue" << endl;
-		return false;
+
+	void spin(){
+		runIOLoop();
+		setForce({ 0, 0, 0 });
+		getFalconFirmware()->setLEDStatus(FalconFirmware::GREEN_LED);
+		runIOLoop();
+
+		ros::AsyncSpinner spinner(1, &cb_queue_);
+		spinner.start();
+
+		geometry_msgs::PoseStamped p;
+		p.pose.orientation.w = 1.0;
+		p.header.frame_id = "falcon_base";
+
+		sensor_msgs::Joy joy_msg;
+		joy_msg.header.frame_id = "falcon_base";
+		joy_msg.buttons.resize(getFalconGrip()->getNumDigitalInputs());
+
+		std::array<double, 3> p_arr;
+
+		ros::Rate rate{ 1000 };
+		while(ros::ok()) {
+			if(runIOLoop()) {
+				auto const now{ ros::Time::now() };
+				p_arr = getPosition();
+				p.pose.position.x = p_arr[0];
+				p.pose.position.y = p_arr[1];
+				p.pose.position.z = p_arr[2];
+				p.header.stamp = now;
+				measured_cp_pub_.publish(p);
+
+				joy_msg.header.stamp = now;
+				joy_msg.axes.resize(3);
+				joy_msg.axes[0] = p_arr[0];
+				joy_msg.axes[1] = p_arr[1];
+				joy_msg.axes[2] = p_arr[2];
+
+				for(size_t i = 0; i < getFalconGrip()->getNumDigitalInputs(); ++i) {
+					joy_msg.buttons[i] = getFalconGrip()->getDigitalInput(i);
+				}
+				joy_pub_.publish(joy_msg);
+			}
+			{
+				std::lock_guard<std::mutex> lock(force_mutex_);
+				setForce(cmd_force_);
+			}
+			rate.sleep();
+		}
+
+		spinner.stop();
 	}
-	cout << "Firmware loaded" << endl;
-    
-    m_falconDevice.getFalconFirmware()->setHomingMode(true); //Set homing mode (keep track of encoders !needed!)
-    cout << "Homing Set" << endl;
-    std::array<int, 3> forces;
-    m_falconDevice.getFalconFirmware()->setForces(forces);
-  	m_falconDevice.runIOLoop(); //read in data  	
-
-  	{
-	    bool stop = false;
-	    bool homing = false;
-	    bool homing_reset = false;
-	    usleep(100000);
-        int tryLoad = 0;
-	    while(!stop && tryLoad < 100)
-	    {
-		    if(!m_falconDevice.runIOLoop()) continue;
-		    if(!m_falconDevice.getFalconFirmware()->isHomed())
-		    {
-			    if(!homing)
-			    {
-				    m_falconDevice.getFalconFirmware()->setLEDStatus(libnifalcon::FalconFirmware::RED_LED);
-				    cout << "Falcon not currently homed. Move control all the way out then push straight all the way in." << endl;
-       	        
-			    }
-			    homing = true;
-		    }
-
-		    if(homing && m_falconDevice.getFalconFirmware()->isHomed())
-		    {
-			    m_falconDevice.getFalconFirmware()->setLEDStatus(libnifalcon::FalconFirmware::BLUE_LED);
-			    cout << "Falcon homed." << endl;
-			    homing_reset = true;
-			    stop = true;
-		    }
-            tryLoad++;
-	    }
-        while(!m_falconDevice.runIOLoop());
-    }
-    return true;	
-}
-
-void forceCallback(const ros_falcon::falconForcesConstPtr& msg)
-{
-    std::array<double,3> forces;
-    forces[0] = msg->X;
-    forces[1] = msg->Y;
-    forces[2] = msg->Z;
-	m_falconDevice.setForce(forces);
-
-    cout << "set Force" << endl;
-    //TODO Add safety to only apply forces for limited time
-}
+};
 
 int main(int argc, char* argv[])
 {
-    ros::init(argc,argv, "ROSfalcon");
-    
-    //TODO Driver currently assumes there is only one falcon attached 
-    if(init_falcon(0))
-    { 
-        cout << "Falcon Initialised Starting ROS Node" << endl;
+    ros::init(argc,argv, "falcon");
+	ros::NodeHandle nh{"~"};
 
+	ros::AsyncSpinner spinner(1);
+	spinner.start();
 
-        m_falconDevice.setFalconKinematic<libnifalcon::FalconKinematicStamper>();
-        ros::NodeHandle node;
-        
-        //Start ROS Subscriber
-        ros::Subscriber sub = node.subscribe("/falconForce", 10, &forceCallback);
-        
-        //Start ROS Publisher
-        ros::Publisher pub = node.advertise<ros_falcon::falconPos>("falconPos",10);        
+	Controller dev{ nh };
 
-        ros::Rate loop_rate(1000);
+	if(!dev.init())
+		return 1;
 
-        while(node.ok())
-	    {
-		    if(m_falconDevice.runIOLoop())
-            {
-		        //////////////////////////////////////////////
-		        //Request the current encoder positions:
-		        std::array<double, 3> Pos;
-		        Pos = m_falconDevice.getPosition();
+	ROS_INFO("device initialized");
 
-                //Publish ROS values
-		        ros_falcon::falconPos position;
-                position.X = Pos[0];
-                position.Y = Pos[1];
-                position.Z = Pos[2];
-                pub.publish(position);
-                
-            }
-            loop_rate.sleep();
-	    }
-        m_falconDevice.close();
-    }
+	if(!dev.home())
+		return 1;
+
+	dev.spin();
 
 	return 0;
 }
